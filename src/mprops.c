0358f841ba98f57a7c51b9434fa5433da231a6ee
Ryan Bourgeois <bluedragonx@gmail.com>
2011-04-27 23:14:19 -0500

    Created mprops.c and mprops.h for xinput properties support.

---
+++ src/mprops.c
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *
+ * Multitouch X driver
+ * Copyright (C) 2011 Ryan Bourgeois <bluedragonx@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **************************************************************************/
+
+#include "mprops.h"
+#include "common.h"
+
+#define MAX_VALUES 4
+
+static void atom_init(DeviceIntPtr dev, char* name, int size, int nvalues int* values) {
+	Atom atom;
+	int i;
+	uint8_t uvals8[MAX_VALUES];
+	uint16_t uvals16[MAX_VALUES];
+	uint32_t uvals32[MAX_VALUES];
+	pointer uvals;
+	nvalues = MINVAL(MAX_VALUES, nvalues);
+
+	switch(size) {
+	case 8:
+		for (i = 0; i < nvalues; i++) {
+			uvals8[i] = values[i];
+		}
+		uvals = uvals8;
+		break;
+	case 16:
+		for (i = 0; i < nvalues; i++) {
+			uvals16[i] = values[i];
+		}
+		uvals = uvals16;
+		break;
+	default:
+		for (i = 0; i < nvalues; i++) {
+			uvals32[i] = values[i];
+		}
+		uvals = uvals32;
+		break;
+	}
+
+	atom = MakeAtom(name, strlen(name), TRUE);
+}
+
+void mprops_init(struct MProps* props, InputInfoPtr local) {
+	
+}
+
