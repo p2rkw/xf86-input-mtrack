1658e03de642c19475622d30487cfc0e2608489b
Ryan Bourgeois <bluedragonx@gmail.com>
2011-05-22 13:20:24 -0500

    Moved code from xbypass.h into test.c. Removed xbypass.h and moved test.c into tools subdirectory.

--- tools/test.c
+++ tools/test.c
@@ -22,7 +22,30 @@
 
 #include <mtouch.h>
 #include <fcntl.h>
-#include <xbypass.h>
+#include <stdarg.h>
+
+void xf86Msg(int type, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+}
+
+int xf86SetIntOption(pointer opts, const char *name, int deflt)
+{
+	return deflt;
+}
+
+int xf86SetBoolOption(pointer opts, const char *name, int deflt)
+{
+	return deflt;
+}
+
+double xf86SetRealOption(pointer opts, const char *name, double deflt)
+{
+	return deflt;
+}
 
 static void print_gestures(const struct Gestures* gs)
 {
